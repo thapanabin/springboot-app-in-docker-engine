a775a55f31ad2de82afa8950df9b53676a7e1359
thapanabin <thapanabincr7@outlook.com>
2020-04-02 19:06:58 -0500

    added project skeleton

---
+++ springboot-app-in-docker/src/main/java/com/nabin/SpringbootAppInDockerApplication.java
@@ -0,0 +1,13 @@
+package com.nabin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAppInDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAppInDockerApplication.class, args);
+	}
+
+}
